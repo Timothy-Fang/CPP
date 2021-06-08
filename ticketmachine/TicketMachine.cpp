031519a1a8ca9f96322e7b377334ee7094b00e0e
Timothy-Fang <532322836@qq.com>
2021-06-08 23:03:16 +0800

    ticket

---
+++ ticketmachine/TicketMachine.cpp
@@ -0,0 +1,18 @@
+/*
+ * TicketMachine.cpp
+ *
+ *  Created on: 2021年6月8日
+ *      Author: timo
+ */
+
+#include "TicketMachine.h"
+
+TicketMachine::TicketMachine() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TicketMachine::~TicketMachine() {
+	// TODO Auto-generated destructor stub
+}
+
