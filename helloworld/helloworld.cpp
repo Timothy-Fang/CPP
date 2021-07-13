2fb7035002b65d35cf6cdfe4ee5f47a1ddf8ecc9
Timothy-Fang <532322836@qq.com>
2021-07-13 21:46:47 +0800

    配置文件

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -7,7 +7,6 @@ using namespace std;
 int main()
 {
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
     for (const string& word : msg)
     {
         cout << word << " ";
