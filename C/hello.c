54e1cd94d42e3cccfb14f13d6a489dff0555e65a
Timothy-Fang <532322836@qq.com>
2021-07-18 23:42:48 +0800

    a+1.0

--- C/hello.c
+++ C/hello.c
@@ -4,7 +4,8 @@ int main()
 {
     int a;
     a = 6;
-    printf("sizeof(int)=%ld\n",sizeof(double));
-    printf("sizeof(a)=%ld\n",sizeof(a));
+    printf("sizeof(int)=%ld\n",sizeof(a));
+    printf("sizeof(a)=%ld\n",sizeof(a+1.0));
+    printf("a=%d\n", a);
     return 0;
 }
